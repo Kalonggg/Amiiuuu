0b51340bd77ed47b0a1cb1bd9965e571825c5d8d
Kalonggg <longhoang260303@gmail.com>
2024-08-24 00:09:31 +0700

    apologyGf

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ami iuuuu à!',
+    introDesc: `Anh yêu em rất nhiều luôn ý, anh biết lỗi rồi ạ anh đáng trách, mong em tha lỗi cho anh, đừng giận anh nữa nha! `,
+    btnIntro: 'Nhaaaaa',
+    title: 'Huyền iuu owi tha thứ cho anh nhóooo 😙',
+    desc: 'Huyền không chọn hoặc thoát là tha lỗi cho anh đó',
+    btnYes: 'Ami sẽ tha thứ cho Long',
+    btnNo: 'Không tha thứ!',
+    question:'Cảm ơn em iuu. Đừng tự ti vì ai nhé, em luôn là số một tronng anh<3. Huyền có yêu anh không?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Nào anh qua đón đi chơi nhé!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
